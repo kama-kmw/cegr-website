1be69eba2d608801bae37ea74b05fef248582cef
kamil <kamil.l2020@yandex.ru>
2022-03-25 20:17:25 +0300

    редактировал моб меню

--- js/elem-show.js
+++ js/elem-show.js
@@ -4,3 +4,10 @@ $(".mob-menu__show").click(function () {
   return false;
 });
 
+
+$(".mob-menu__show-sub").click(function () {
+  $(this).parent().find(".quicklinks-sub__item").slideToggle(300);
+  $(this).find(".sp__arrow-d").toggleClass("sp__arrow-u");
+  return false;
+});
+
