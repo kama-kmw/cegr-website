12b7454d7449ac3cb38813468cf853380dbb38eb
Surha <surha_dag@mail.ru>
2022-03-25 20:06:50 +0300

    add includes

---
+++ includes/categories-section.php
@@ -0,0 +1,138 @@
+<!-- categories section -->
+<section class="categories-section spad">
+    <div class="container">
+        <div class="section-title">
+            <h2>О журнале</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris
+                scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.</p>
+        </div>
+        <div class="row categories-items">
+            <!-- categorie -->
+            <div class="categorie__item-box">
+                <div class="categorie-item">
+                    <div class="ci-thumb set-bg" data-setbg="img/categories/1.jpg"></div>
+                    <div class="ci-text">
+                        <!-- <h5>IT Development</h5> -->
+                        <p>Журнал "Экономические и гуманитарные исследования регионов" прошел экспертизу и
+                            подтвердил право быть включенным в Перечень Вак </p>
+                        <!-- <a href="http://vak.ed.gov.ru/documents/10179/0/Перечень%20ВАК_02.10.2017.pdf/30d136d1-42b5-4c8f-8c79-a76d04132245"><span>перейти</span></a> -->
+                    </div>
+                </div>
+            </div>
+            <!-- categorie -->
+            <div class="categorie__item-box">
+                <div class="categorie-item">
+                    <div class="ci-thumb set-bg" data-setbg="img/categories/2.jpg"></div>
+                    <div class="ci-text">
+                        <!-- <h5>Web Design</h5> -->
+                        <p>Журнал «Экономические и гуманитарные исследования регионов» включен в Новый Перечень
+                            рецензируемых научных изданий, в которых должны быть опубликованы основные научные
+                            результаты диссертаций на соискание ученой степени кандидата наук, на соискание ученой
+                            степени доктора наук и публикует материалы оригинальных теоретических и прикладных
+                            исследований по актуальным проблемам педагогики, философии и социологии, представляющие
+                            интерес для профессорско-преподавательского состава и научных работников. </p>
+                        <!-- <a href="http://perechen.vak2.ed.gov.ru/"><span>перейти</span></a> -->
+                    </div>
+                </div>
+            </div>
+            <!-- categorie -->
+            <div class="categorie__item-box">
+                <div class="categorie-item">
+                    <div class="ci-thumb set-bg" data-setbg="img/categories/3.jpg"></div>
+                    <div class="ci-text">
+                        <!-- <h5>Illustration & Drawing</h5> -->
+                        <p>Журнал издается в соответствии с Федеральным законодательством о средствах массовой
+                            информации, зарегистрирован в Федеральной службе по надзору в сфере связи,
+                            информационных технологий и массовых коммуникаций (Роскомнадзор) – регистрационный номер
+                            ПИ № ФСС77-39740. Подписной индекс в «Объединенном каталоге. Пресса России. Газеты и
+                            журналы» - 37137. Правила публикации для авторов разработаны редколлегией и утверждены
+                            главным редактором. </p>
+                        <!-- <span>55 Courses</span> -->
+                    </div>
+                </div>
+            </div>
+            <!-- categorie -->
+            <div class="categorie__item-box">
+                <div class="categorie-item">
+                    <div class="ci-thumb set-bg" data-setbg="img/categories/4.jpg"></div>
+                    <div class="ci-text">
+                        <!-- <h5>Social Media</h5> -->
+                        <p>Журнал издается типографским способом в печатной форме, выходит 1 раз в два месяца, имеет
+                            электронную полнотекстовую версию, которая по содержанию идентична печатной форме,
+                            размещается в сети Интернет после выхода очередного номера по адресу</p>
+                        <!-- <a href="http://cegr.ru/index.php"><span>www.cegr.ru</span></a> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="categories-section spad lang">
+    <div class="container">
+        <div class="section-title">
+            <h2>О журнале</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris
+                scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.</p>
+        </div>
+        <div class="row">
+            <!-- categorie -->
+            <div class="col-lg-4 col-md-6">
+                <div class="categorie-item">
+                    <div class="ci-thumb set-bg" data-setbg="img/categories/1.jpg"></div>
+                    <div class="ci-text">
+                        <h5>IT Development</h5>
+                        <p>Журнал "Экономические и гуманитарные исследования регионов" прошел экспертизу и
+                            подтвердил право быть включенным в Перечень Вак </p>
+                        <a href="http://vak.ed.gov.ru/documents/10179/0/Перечень%20ВАК_02.10.2017.pdf/30d136d1-42b5-4c8f-8c79-a76d04132245"><span>перейти</span></a>
+                    </div>
+                </div>
+            </div>
+            <!-- categorie -->
+            <div class="col-lg-4 col-md-6">
+                <div class="categorie-item">
+                    <div class="ci-thumb set-bg" data-setbg="img/categories/2.jpg"></div>
+                    <div class="ci-text">
+                        <h5>Web Design</h5>
+                        <p>Журнал «Экономические и гуманитарные исследования регионов» включен в Новый Перечень
+                            рецензируемых научных изданий, в которых должны быть опубликованы основные научные
+                            результаты диссертаций на соискание ученой степени кандидата наук, на соискание ученой
+                            степени доктора наук и публикует материалы оригинальных теоретических и прикладных
+                            исследований по актуальным проблемам педагогики, философии и социологии, представляющие
+                            интерес для профессорско-преподавательского состава и научных работников. </p>
+                        <a href="http://perechen.vak2.ed.gov.ru/"><span>перейти</span></a>
+                    </div>
+                </div>
+            </div>
+            <!-- categorie -->
+            <div class="col-lg-4 col-md-6">
+                <div class="categorie-item">
+                    <div class="ci-thumb set-bg" data-setbg="img/categories/3.jpg"></div>
+                    <div class="ci-text">
+                        <h5>Illustration & Drawing</h5>
+                        <p>Журнал издается в соответствии с Федеральным законодательством о средствах массовой
+                            информации, зарегистрирован в Федеральной службе по надзору в сфере связи,
+                            информационных технологий и массовых коммуникаций (Роскомнадзор) – регистрационный номер
+                            ПИ № ФСС77-39740. Подписной индекс в «Объединенном каталоге. Пресса России. Газеты и
+                            журналы» - 37137. Правила публикации для авторов разработаны редколлегией и утверждены
+                            главным редактором. </p>
+                        <!-- <span>55 Courses</span> -->
+                    </div>
+                </div>
+            </div>
+            <!-- categorie -->
+            <div class="col-lg-4 col-md-6">
+                <div class="categorie-item">
+                    <div class="ci-thumb set-bg" data-setbg="img/categories/4.jpg"></div>
+                    <div class="ci-text">
+                        <h5>Social Media</h5>
+                        <p>Журнал издается типографским способом в печатной форме, выходит 1 раз в два месяца, имеет
+                            электронную полнотекстовую версию, которая по содержанию идентична печатной форме,
+                            размещается в сети Интернет после выхода очередного номера по адресу</p>
+                        <a href="http://cegr.ru/index.php"><span>www.cegr.ru</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- categories section end -->
---
+++ includes/hero-section.php
@@ -0,0 +1,34 @@
+<!-- Hero section -->
+<section class="hero-section set-bg" data-setbg="img/bg.jpg">
+	<div class="container">
+		<div class="hero-text text-white">
+			<h2 class="hero-section__h2">Экономические и гуманитарные исследования регионов</h2>
+		</div>
+		<!-- <div class="row">
+				<div class="col-lg-10 offset-lg-1">
+					<form class="intro-newslatter">
+						<input type="text" placeholder="Name">
+						<input type="text" class="last-s" placeholder="E-mail">
+						<button class="site-btn">Sign Up Now</button>
+					</form>
+				</div>
+			</div> -->
+	</div>
+</section>
+<section class="hero-section set-bg lang" data-setbg="img/bg.jpg">
+	<div class="container">
+		<div class="hero-text text-white">
+			<h2>Экономические и гуманитарные исследования регионов</h2>
+		</div>
+		<!-- <div class="row">
+				<div class="col-lg-10 offset-lg-1">
+					<form class="intro-newslatter">
+						<input type="text" placeholder="Name">
+						<input type="text" class="last-s" placeholder="E-mail">
+						<button class="site-btn">Sign Up Now</button>
+					</form>
+				</div>
+			</div> -->
+	</div>
+</section>
+<!-- Hero section end -->
