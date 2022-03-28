07bdbe3d92da98678a4b1acd850ceaa720cbccc3
Surha <surha_dag@mail.ru>
2022-03-29 02:55:32 +0300

    добавил англ в моб меню

--- includes/header.php
+++ includes/header.php
@@ -167,61 +167,90 @@
                 <div class="toggle" onclick="toggle()"></div>
                 <div class="mob-container">
                     <ul class="mob-menu">
-                        <li class="mob-menu__item logo">
+                        <li class="mob-menu__item">
                             <a href="/" class="mob-menu__link">Главная</a>
+                            <a href="/" class="mob-menu__link lang-en">Main</a>
                         </li>
                         <li class="mob-menu__item">
                             <a href="redcolegiya.php" class="mob-menu__link">Главный редактор, редколегия и редсовет</a>
+                            <a href="redcolegiya.php" class="main-menu__link lang-en">Editorial Board</a>
                         </li>
                         <li class="mob-menu__item">
                             <a href="#" class="mob-menu__link mob-menu__show">Нормативно-правовая база <span class="sp__arrow-d"></span></a>
+                            <a href="#" class="main-menu__link lang-en">Regulatory framework</a>
                             <ul>
-                                <li class="quicklinks-item"><a href="smi.php">Закон о средствах массовой информации</a>
+                                <li class="quicklinks-item">
+                                    <a href="smi.php">Закон о средствах массовой информации</a>
+                                    <a href="smi.php" class="main-menu__link lang-en">The Law on Mass Media</a>
                                 </li>
                                 <li class="quicklinks-item">
                                     <a href="about-journal.php">Положение о журнале</a>
+                                    <a href="about-journal.php" class="main-menu__link lang-en">Regulations on the journal</a>
                                 </li>
                                 <li class="quicklinks-item">
                                     <a href="rules.php">Правила направления, рецензирования и опубликование научных
                                         статей</a>
+                                        <a href="rules.php" class="main-menu__link lang-en">Review rules</a>
                                 </li>
                             </ul>
                         </li>
                         <li class="mob-menu__item">
                             <a href="#" class="mob-menu__link mob-menu__show">Авторам <span class="sp__arrow-d"></span></a>
+                            <a href="#" class="main-menu__link lang-en">To the authors</a>
                             <ul>
-                                <li class="quicklinks-item"><a href="#">Авторам</a></li>
+                                <li class="quicklinks-item">
+                                    <a href="#">Правила оформления научных статей</a>
+                                    <a href="article-rule.php" class="main-menu__link lang-en">Rules for the design of scientific articles</a>
+                                </li>
                                 <li class="quicklinks-item">
                                     <a href="license-agreement.php">Договор о передачи прав на опубликование</a>
+                                    <a href="license-agreement.php" class="main-menu__link lang-en">Agreement on the transfer of publishing rights</a>
                                 </li>
                                 <li class="quicklinks-item">
                                     <a href="#">Разрешение на опубликование индивидуальных сведений об авторе</a>
+                                    <a href="agrement.php" class="main-menu__link lang-en">Permission to publish individual information about the author</a>
                                 </li>
                                 <li class="quicklinks-item">
                                     <a href="#">Сведения об авторе</a>
+                                    <a href="about-author.php" class="main-menu__link lang-en">Information about the author</a>
                                 </li>
                                 <li class="quicklinks-item">
                                     <a href="recenzia.php">Резенция</a>
+                                    <a href="recenzia.php" class="main-menu__link lang-en">Review</a>
                                 </li>
                                 <li class="quicklinks-item">
                                     <a href="#">Этический кодекс</a>
+                                    <a href="etic-code.php" class="main-menu__link lang-en">Code of Ethics</a>
                                 </li>
                             </ul>
                         </li>
                         <li class="mob-menu__item">
                             <a href="#" class="mob-menu__link mob-menu__show">Архивы <span class="sp__arrow-d"></span></a>
+                            <a href="#" class="main-menu__link lang-en">Archives</a>
                             <ul>
                                 <li class="quicklinks-item">
                                     <a href="#" class='mob-menu__show-sub'>2019
                                         <span class="sp__arrow-d"></span>
                                     </a>
                                     <ul>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_1_2019.pdf" target="_blank">Выпуск №1</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_2_2019.pdf" target="_blank">Выпуск №2</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_3_2019.pdf" target="_blank">Выпуск №3</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_4_2019.pdf" target="_blank">Выпуск №4</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_5_2019.pdf" target="_blank">Выпуск №5</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_6_2019.pdf" target="_blank">Выпуск №6</a></li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_1_2019.pdf" target="_blank">Выпуск №1</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_2_2019.pdf" target="_blank">Выпуск №2</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_3_2019.pdf" target="_blank">Выпуск №3</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_4_2019.pdf" target="_blank">Выпуск №4</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_5_2019.pdf" target="_blank">Выпуск №5</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_6_2019.pdf" target="_blank">Выпуск №6</a>
+                                        </li>
                                     </ul>
                                 </li>
                                 <li class="quicklinks-item">
@@ -229,12 +258,24 @@
                                         <span class="sp__arrow-d"></span>
                                     </a>
                                     <ul>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_1_2020.pdf" target="_blank">Выпуск №1</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_2_2020.pdf" target="_blank">Выпуск №2</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_3_2020.pdf" target="_blank">Выпуск №3</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_4_2020.pdf" target="_blank">Выпуск №4</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_5_2020.pdf" target="_blank">Выпуск №5</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_6_2020.pdf" target="_blank">Выпуск №6</a></li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_1_2020.pdf" target="_blank">Выпуск №1</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_2_2020.pdf" target="_blank">Выпуск №2</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_3_2020.pdf" target="_blank">Выпуск №3</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_4_2020.pdf" target="_blank">Выпуск №4</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_5_2020.pdf" target="_blank">Выпуск №5</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_6_2020.pdf" target="_blank">Выпуск №6</a>
+                                        </li>
                                     </ul>
                                 </li>
                                 <li class="quicklinks-item">
@@ -242,12 +283,24 @@
                                         <span class="sp__arrow-d"></span>
                                     </a>
                                     <ul>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_1_2021.pdf" target="_blank">Выпуск №1</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_2_2021.pdf" target="_blank">Выпуск №2</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_3_2021.pdf" target="_blank">Выпуск №3</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_4_2021.pdf" target="_blank">Выпуск №4</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_5_2021.pdf" target="_blank">Выпуск №5</a></li>
-                                        <li class='quicklinks-sub__item'><a href="docs/journal_6_2021.pdf" target="_blank">Выпуск №6</a></li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_1_2021.pdf" target="_blank">Выпуск №1</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_2_2021.pdf" target="_blank">Выпуск №2</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_3_2021.pdf" target="_blank">Выпуск №3</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_4_2021.pdf" target="_blank">Выпуск №4</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_5_2021.pdf" target="_blank">Выпуск №5</a>
+                                        </li>
+                                        <li class='quicklinks-sub__item'>
+                                            <a href="docs/journal_6_2021.pdf" target="_blank">Выпуск №6</a>
+                                        </li>
                                     </ul>
                                 </li>
                             </ul>
@@ -255,6 +308,7 @@
 
                         <li class="mob-menu__item">
                             <a href="retraction.php" class="mob-menu__link">Ретракция</a>
+                            <a href="retraction.php" class="main-menu__link lang-en">Retraction</a>
                         </li>
                         <li class="js-change-lang rus">
                             <span class="lang-en"><img src="img/logo/eng-flag.png"></span>
