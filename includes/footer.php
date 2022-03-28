6ccc196883f6761def97b48593b51ace9f6fbf28
Surha <surha_dag@mail.ru>
2022-03-29 02:23:01 +0300

    добавил англ в footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,93 +1,41 @@
 <!-- footer section -->
 <footer class="footer-section spad pb-0 banner-fon">
-	<div class="footer-top">
-		<!-- <div class="footer-warp">
-				<div class="row">
-					<!-- <div class="widget-item">
-						<h4>Contact Info</h4>
-						<ul class="contact-list">
-							<li>1481 Creekside Lane <br>Avila Beach, CA 931</li>
-							<li>+53 345 7953 32453</li>
-							<li>yourmail@gmail.com</li>
-						</ul>
-					</div> -->
-		<!-- <div class="widget-item">
-						<h4>Engeneering</h4>
-						<ul>
-							<li><a href="">Applied Studies</a></li>
-							<li><a href="">Computer Engeneering</a></li>
-							<li><a href="">Software Engeneering</a></li>
-							<li><a href="">Informational Engeneering</a></li>
-							<li><a href="">System Engeneering</a></li>
-						</ul>
-					</div> -->
-		<!-- <div class="widget-item">
-						<h4>Graphic Design</h4>
-						<ul>
-							<li><a href="">Applied Studies</a></li>
-							<li><a href="">Computer Engeneering</a></li>
-							<li><a href="">Software Engeneering</a></li>
-							<li><a href="">Informational Engeneering</a></li>
-							<li><a href="">System Engeneering</a></li>
-						</ul>
-					</div> -->
-		<!-- <div class="widget-item">
-						<h4>Development</h4>
-						<ul>
-							<li><a href="">Applied Studies</a></li>
-							<li><a href="">Computer Engeneering</a></li>
-							<li><a href="">Software Engeneering</a></li>
-							<li><a href="">Informational Engeneering</a></li>
-							<li><a href="">System Engeneering</a></li>
-						</ul>
-					</div> -->
-		<!-- <div class="widget-item">
-						<h4>Newsletter</h4>
-						<form class="footer-newslatter">
-							<input type="email" placeholder="E-mail">
-							<button class="site-btn">Subscribe</button>
-							<p>*We don’t spam</p>
-						</form>
-					</div> -->
-	</div>
-	</div>
 	<div class="footer-contact">
-			<h3 class="footer-contact__subtitle">
-				Контактная информация
-			</h3>
-			<p class="footer-contact__text">
-				Адрес: 357500, Россия, Ставропольский край, г.Пятигорск, ул. Ленина , 116, к.А <br>
-				Электронная почта: <a href="#" class="footer-contact__link">redsov@mail.ru</a>
-			</p>
-			<script type="text/javascript">
-				new Image().src = "//counter.yadro.ru/hit?r" +
-					escape(document.referrer) + ((typeof(screen) == "undefined") ? "" :
-						";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
-							screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
-					";h" + escape(document.title.substring(0, 150)) +
-					";" + Math.random();
-			</script>
-			<h3 class="footer-contact__subtitle">Счетчик посещений на сайте:</h3>
-			<p class="footer-contact__text">
+		<h3 class="footer-contact__subtitle lang-ru">
+			Контактная информация
+		</h3>
+		<h3 class="footer-contact__subtitle lang-en">
+		Contact information
+		</h3>
+		<p class="footer-contact__text lang-ru">
+			Адрес: 357500, Россия, Ставропольский край, г.Пятигорск, ул. Ленина , 116, к.А <br>
+			Электронная почта: <a href="#" class="footer-contact__link">redsov@mail.ru</a>
+		</p>
+		<p class="footer-contact__text lang-en">
+		Address: 116 Lenin str., k.A, 357500, Stavropol Territory, Russia, Pyatigorsk <br>
+		Email: <a href="#" class="footer-contact__link">redsov@mail.ru</a>
+		</p>
+		<script type="text/javascript">
+			new Image().src = "//counter.yadro.ru/hit?r" +
+				escape(document.referrer) + ((typeof(screen) == "undefined") ? "" :
+					";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
+						screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
+				";h" + escape(document.title.substring(0, 150)) +
+				";" + Math.random();
+		</script>
+		<h3 class="footer-contact__subtitle lang-ru">Счетчик посещений на сайте:</h3>
+		<h3 class="footer-contact__subtitle lang-en">The counter of visits on the site:</h3>
+		<p class="footer-contact__text">
 			<a href="//www.liveinternet.ru/click" target="_blank"><img src="//counter.yadro.ru/logo?28.12" title="LiveInternet: показано количество просмотров и посетителей" alt="" border="0" height="160" width="120"></a>
-			</p>
-			
-		</div>
+		</p>
 	</div>
-	</div>
-
 	<div class="footer-bottom">
 		<div class="footer-warp">
-			<!-- <ul class="footer-menu">
-					<li><a href="#">Terms & Conditions</a></li>
-					<li><a href="#">Register</a></li>
-					<li><a href="#">Privacy</a></li>
-				</ul> -->
 			<div class="copyrightText">
-				<p class="copyrightText__text">Copyright © 2022. Сайт создан на <a href="http://art-cod.ru">Art-Cod.ru</a></p>
+				<p class="copyrightText__text lang-ru">Все права защищены © 2022. Сайт создан на <a href="http://art-cod.ru" target="_blank">Art-Cod.ru</a></p>
+				<p class="copyrightText__text lang-en">Copyright © 2022. The site was created on <a href="http://art-cod.ru" target="_blank">Art-Cod.ru</a></p>
 			</div>
 		</div>
 	</div>
-
 </footer>
 <!-- footer section end -->
