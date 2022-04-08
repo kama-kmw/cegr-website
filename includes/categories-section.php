e6952cd76940fa6e601429e0166a0f564ac07d69
Surha <surha_dag@mail.ru>
2022-04-08 11:33:07 +0300

    Изменил текст на главной

--- includes/categories-section.php
+++ includes/categories-section.php
@@ -1,13 +1,12 @@
 <!-- categories section -->
 <section class="categories-section spad">
-    <div class="container">
-        <div class="section-title">
+    <div class="container ">
+        <div class="section-title text-justify">
             <div class="lang-ru">
                 <h2>О журнале</h2>
-                <p>Журнал "Экономические и гуманитарные исследования регионов" прошел экспертизу и подтвердил право быть включенным в Перечень ВАК: <a href="http://vak.ed.gov.ru/documents/10179/0/%CF%E5%F0%E5%F7%E5%ED%FC%20%C2%C0%CA_02.10.2017.pdf/30d136d1-42b5-4c8f-8c79-a76d04132245" target="_blank">Ссылка</a></p>
-                <p>Журнал «Экономические и гуманитарные исследования регионов» включен в Новый Перечень рецензируемых научных изданий, в которых должны быть опубликованы основные научные результаты диссертаций на соискание ученой степени кандидата наук, на соискание ученой степени доктора наук (http://perechen.vak2.ed.gov.ru/) и публикует материалы оригинальных теоретических и прикладных исследований по актуальным проблемам педагогики, философии и социологии, представляющие интерес для профессорско-преподавательского состава и научных работников.</p>
-                <p>Журнал издается в соответствии с Федеральным законодательством о средствах массовой информации, зарегистрирован в Федеральной службе по надзору в сфере связи, информационных технологий и массовых коммуникаций (Роскомнадзор) – регистрационный номер ПИ № ФСС77-39740. Подписной индекс в «Объединенном каталоге. Пресса России. Газеты и журналы» - 37137. Правила публикации для авторов разработаны редколлегией и утверждены главным редактором. </p>
-                <p>Журнал издается типографским способом в печатной форме, выходи 1 раз в два месяца, имеет электронную полнотекстовую версию, которая по содержанию идентична печатной форме, размещается в сети Интернет после выхода очередного номера по адресу <a href="http://cegr.ru" target="_blank">www.cegr.ru</a></p>
+                <p>Журнал «Экономические и гуманитарные исследования регионов» включен в Перечень рецензируемых научных изданий ВАК , в которых должны быть опубликованы основные научные результаты диссертаций на соискание ученой степени кандидата наук, на соискание ученой степени доктора наук и публикует материалы оригинальных теоретических и прикладных исследований по актуальным проблемам педагогики, философии и социологии, представляющие интерес для профессорско-преподавательского состава и научных работников.</p>
+                <p>Журнал издается в соответствии с Федеральным законодательством о средствах массовой информации, зарегистрирован в Федеральной службе по надзору в сфере связи, информационных технологий и массовых коммуникаций (Роскомнадзор) – регистрационный номер ПИ № ФСС77-39740. Правила публикации для авторов разработаны редколлегией и утверждены главным редактором.</p>
+                <p>Журнал издается типографским способом в печатной форме, выходит      1 раз в 2 месяца, имеет электронную полнотекстовую версию, которая по содержанию идентична печатной форме и  размещается в сети Интернет после выхода очередного номера по адресу <a href="http://cegr.ru" target="_blank">www.cegr.ru</a></p>
 
                 <h3 class="mt-2 mb-1">Тематические направления</h3>
                 <p>09.00.01 – Онтология и теория познания (философские науки)</p>
@@ -23,10 +22,9 @@
             </div>
             <div class="lang-en">
                 <h2>About the journal</h2>
-                <p>The journal "Economic and Humanitarian Studies of Regions" has passed the examination and confirmed the right to be included in the List of the Higher Attestation Commission: <a href="http://vak.ed.gov.ru/documents/10179/0/%CF%E5%F0%E5%F7%E5%ED%FC%20%C2%C0%CA_02.10.2017.pdf/30d136d1-42b5-4c8f-8c79-a76d04132245" target="_blank">Link</a></p>
-                <p>The journal "Economic and Humanitarian Studies of Regions" is included in the New List of peer-reviewed scientific publications in which the main scientific results of dissertations for the degree of Candidate of Sciences, for the degree of Doctor of Sciences should be published (http://perechen.vak2.ed.gov.ru /) and publishes materials of original theoretical and applied research on topical issues of pedagogy, philosophy and sociology, representing of interest to the teaching staff and researchers.</p>
-                <p>The journal is published in accordance with Federal legislation on Mass Media, registered with the Federal Service for Supervision of Communications, Information Technology and Mass Communications (Roskomnadzor) - registration number PI No. FSS77-39740. Subscription index in the "Combined catalog. The press of Russia. Newspapers and magazines" - 37137. The publication rules for authors were developed by the editorial board and approved by the editor-in-chief. </p>
-                <p>The journal is published typographically in printed form, published 1 time every two months, has an electronic full-text version, which is identical in content to the printed form, is posted on the Internet after the next issue is published at <a href="http://cegr.ru" target="_blank">www.cegr.ru</a></p>
+                <p>The journal "Economic and Humanitarian Studies of Regions" is included in the List of peer-reviewed scientific publications of the Higher Attestation Commission, in which the main scientific results of dissertations for the degree of Candidate of Sciences, for the degree of Doctor of Sciences should be published and publishes materials of original theoretical and applied research on topical issues of pedagogy, philosophy and sociology of interest to the professorialteaching staff and researchers.</p>
+                <p>The journal is published in accordance with Federal legislation on Mass Media, registered with the Federal Service for Supervision of Communications, Information Technology and Mass Communications (Roskomnadzor) – registration number PI No. FSS77-39740. The publication rules for authors were developed by the editorial board and approved by the editor-in-chief.</p>
+                <p>The journal is published typographically in printed form, is published 1 time in 2 months, has an electronic full-text version, which is identical in content to the printed form and is posted on the Internet after the next issue is published at <a href="http://cegr.ru" target="_blank">www.cegr.ru</a></p>
 
                 <h3 class="mt-2 mb-1">Thematic areas</h3>
                 <p>09.00.01 – Ontology and theory of knowledge (philosophical sciences)</p>
